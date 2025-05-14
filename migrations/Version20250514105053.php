b0c70f40be4c9c579da20843ead97216139d9595
Hasintsoa <hasinaramalanjaona@gmail.com>
2025-05-14 13:52:06 +0300

    Ajout de l'entite Article avec CRUD

---
+++ migrations/Version20250514105053.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250514105053 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE article (id SERIAL NOT NULL, title VARCHAR(200) NOT NULL, content TEXT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))
+        SQL);
+        $this->addSql(<<<'SQL'
+            COMMENT ON COLUMN article.created_at IS '(DC2Type:datetime_immutable)'
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE article_category (article_id INT NOT NULL, category_id INT NOT NULL, PRIMARY KEY(article_id, category_id))
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE INDEX IDX_53A4EDAA7294869C ON article_category (article_id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE INDEX IDX_53A4EDAA12469DE2 ON article_category (category_id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE article_category ADD CONSTRAINT FK_53A4EDAA7294869C FOREIGN KEY (article_id) REFERENCES article (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE article_category ADD CONSTRAINT FK_53A4EDAA12469DE2 FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE SCHEMA public
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE article_category DROP CONSTRAINT FK_53A4EDAA7294869C
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE article_category DROP CONSTRAINT FK_53A4EDAA12469DE2
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE article
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE article_category
+        SQL);
+    }
+}
