4f3998f1025fa9e0717d28d8dcbed3e572d0f1e9
Hasintsoa <hasinaramalanjaona@gmail.com>
2025-05-28 12:52:04 +0300

    initiation du requete http

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mercure\HubInterface;
 use Symfony\Component\Mercure\Update;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 final class HomeController extends AbstractController
 {
@@ -36,5 +37,28 @@ final class HomeController extends AbstractController
         $update = new Update("http://localhost:8000/ping", "0TestPing0");
         $this->hub->publish($update);
         return $this->redirectToRoute('app_article_home');
+    }
+
+    #[Route(path: '/devise', name: 'devise_conversion', methods: ['GET','POST'])]
+    public function deviseConversion(HttpClientInterface $httpClient): Response
+    {
+        $date = (new \DateTimeImmutable())->format('m/d/Y');
+        $response = $httpClient->request('GET', 'https://www.visa.fr/cmsapi/fx/rates', [
+            'headers' => [
+                'Referer' => 'https://www.visa.fr/aide-visa/consommateur/services-de-voyage-visa/exchange-rate-calculator.html'
+            ],
+            'query' => [
+                'amount' => '25000',
+                'fee' => '2',
+                'utcConvertedDate' => $date,
+                'exchangedate' => $date,
+                'fromCurr' => 'EUR',
+                'toCurr' => 'MGA',
+            ]
+        ]);
+        $content = json_decode($response->getContent(), true);
+        return $this->render('home/devise_conversion.html.twig', [
+            'response' => $content,
+        ]);
     }
 }
