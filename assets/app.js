051cfb68438f45c0f1f23043107cd0259bd86485
Hasintsoa <hasinaramalanjaona@gmail.com>
2025-06-02 14:36:59 +0300

    Redesign Blog

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,6 @@
 import "./styles/app.scss";
 import "bootstrap";
-import {Tooltip} from "bootstrap";
+import { Tooltip } from "bootstrap";
 
 document.addEventListener("DOMContentLoaded", () => {
   const tooltipTriggerList = [].slice.call(
@@ -9,4 +9,14 @@ document.addEventListener("DOMContentLoaded", () => {
   tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new Tooltip(tooltipTriggerEl);
   });
+});
+
+window.addEventListener("scroll", () => {
+  document.querySelector(".scroll-top").classList.toggle("active", window.scrollY > 300);
+});
+
+document.querySelector("#scroll-top-btn").addEventListener("click", (event) => {
+  event.preventDefault();
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 });
